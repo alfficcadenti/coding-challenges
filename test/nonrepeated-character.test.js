83de519a48849f4d7e231f485dd136c144728007
Alfredo Ficcadenti <alfredoficcadenti@gmail.com>
2018-11-18 18:27:19 +0000

    added nonrepeated-char test file complete

--- test/nonrepeated-character.test.js
+++ test/nonrepeated-character.test.js
@@ -3,7 +3,16 @@ const firstNonRepeatedCharacter = require("../src/nonrepeated-character");
 
 describe("firstNonRepeatedCharacter", function() {
   it("should return 'A' if given 'ABCD'", function() {
-       // TODO: Fill in this test and add others
+       assert.equal(firstNonRepeatedCharacter("ABCD"), 'A');
+  });
+  it("should return 'sorry' if given 'XXXXX'", function() {
+       assert.equal(firstNonRepeatedCharacter("XXXXX"), 'sorry');
+  });
+  it("should return 'L' if given 'ALABAMA'", function() {
+       assert.equal(firstNonRepeatedCharacter("ALABAMA"), 'L');
+  });
+  it("should return 'sorry' if given 'BABA'", function() {
+       assert.equal(firstNonRepeatedCharacter("BABA"), 'sorry');
   });
 
 });
